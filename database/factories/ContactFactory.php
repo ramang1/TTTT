02a06fc4ba2cd3a312645c738e7b2ce17318f955
Do Thuy <dothuy@MacBook-Air-cua-Do.local>
2020-07-22 17:40:38 +0700

    sua 1 so opt trong contact

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -8,10 +8,10 @@ use Faker\Generator as Faker;
 $factory->define(Contact::class, function (Faker $faker) {
 
     return [
-        'name' => $faker->text,
-        'phone' => $faker->text,
-        'fax' => $faker->text,
-        'mobile' => $faker->text,
+        'name' => $faker->unique()->name,
+        'phone' => $faker->tollFreePhoneNumber,
+        'fax' => $faker->tollFreePhoneNumber,
+        'mobile' => $faker->tollFreePhoneNumber,
         'note' => $faker->text,
         'created_at' => $faker->date('Y-m-d H:i:s'),
         'updated_at' => $faker->date('Y-m-d H:i:s')
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(App\Models\Contact::class, 1000)->create();
     }
 }
