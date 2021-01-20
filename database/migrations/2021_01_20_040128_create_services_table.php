6b12343111c3038714ef09bf871a1a61f6c76d23
DoThuy <comkhechunhat@gmail.com>
2021-01-20 14:26:46 +0700

    Tao bang service de theo doi cac tien trinh

---
+++ database/migrations/2021_01_20_040128_create_services_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('services');
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->text('name')->comment = "Tên dịch vụ";            
+            $table->text('path')->comment = "Đường dẫn thực thi";            
+            $table->text('note')->nullable()->comment = "Mô tả về dịch vụ";
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
