96e4023a0e610b4b7860dc303cc91b147bd0ea3c
famfu79 <49085228+famfu79@users.noreply.github.com>
2021-01-28 17:26:57 +0700

    Thay file .css + .js (LAN 1)

--- resources/views/layouts/datatables_css.blade.php
+++ resources/views/layouts/datatables_css.blade.php
@@ -1,4 +1,4 @@
 <!-- DataTable Bootstrap -->
-<link href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.bootstrap.min.css">
-<link rel="stylesheet" href="https://adminlte.io/themes/AdminLTE/bower_components/bootstrap-daterangepicker/daterangepicker.css">
+<link href="{{asset('backend/css/dataTables.bootstrap.min.css')}}" rel="stylesheet">
+<link rel="stylesheet" href="{{asset('backend/css/buttons.bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('backend/css/daterangepicker1.css')}}">
