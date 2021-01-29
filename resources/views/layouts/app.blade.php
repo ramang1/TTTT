bb407611ae76f80824e130755ba49b8df33e77c1
famfu79 <49085228+famfu79@users.noreply.github.com>
2021-01-29 16:47:42 +0700

    Thay file .css + .js (Phan 2)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -155,16 +155,16 @@
     @endif
 
     <!-- jQuery 3.1.1 -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
-    <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+    <script src="{{asset('backend/js/app_blade_jquery.min.js')}}"></script>
+    <script src="{{asset('backend/js/app_blade_moment.min.js')}}"></script>
+    <script src="{{asset('backend/js/app_blade_bootstrap.min.js')}}"></script>
+    <script src="{{asset('backend/js/app_blade_bootstrap-datetimepicker.min.js')}}"></script>
+    <script src="{{asset('backend/js/app_blade_bootstrap-toggle.min.js')}}"></script>
     <!-- AdminLTE App -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.3/js/adminlte.min.js"></script>
+    <script src="{{asset('backend/js/app_blade_adminlte.min.js')}}"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.min.js"></script>
+    <script src="{{asset('backend/js/app_blade_icheck.min.js')}}"></script>
+    <script src="{{asset('backend/js/app_blade_select2.min.js')}}"></script>
 
     @stack('scripts')
 </body>
--- resources/views/layouts/datatables_js.blade.php
+++ resources/views/layouts/datatables_js.blade.php
@@ -1,19 +1,19 @@
 <!-- Datatables -->
-<script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.bootstrap.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.colVis.min.js"></script>
-<script src="https://adminlte.io/themes/AdminLTE/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="{{asset('backend/js/datatables_js_blade_jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('backend/js/datatables_js_blade_dataTables.bootstrap.min.js')}}"></script>
+<script src="{{asset('backend/js/datatables_js_blade_dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('backend/js/datatables_js_blade_buttons.bootstrap.min.js')}}"></script>
+<script src="{{asset('backend/js/datatables_js_blade_buttons.colVis.min.js')}}"></script>
+<script src="{{asset('backend/js/datatables_js_blade_daterangepicker.js')}}"></script>
 <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
 
 
 
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+<script src="{{asset('backend/js/datatables_js_blade_buttons.flash.min.js')}}"></script>
+<script src="{{asset('backend/js/datatables_js_blade_jszip.min.js')}}"></script>
+<script src="{{asset('backend/js/datatables_js_blade_pdfmake.min.js')}}"></script>
+<script src="{{asset('backend/js/datatables_js_blade_vfs_fonts.js')}}"></script>
+<script src="{{asset('backend/js/datatables_js_blade_buttons.html5.min.js')}}"></script>
+<script src="{{asset('backend/js/datatables_js_blade_buttons.print.min.js')}}"></script>
 
 
